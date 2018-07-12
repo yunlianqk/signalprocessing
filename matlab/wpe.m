d8df6b86fa11f69ab8829b9f5663c00ac687ecdc
Cong Fu <congfu@mobvoi.com>
2018-07-12 09:48:16 +0800

    Update wpe block and streaming process

--- matlab/wpe.m
+++ matlab/wpe.m
@@ -2,12 +2,12 @@ clear all;
 
 frameLen = 10;
 
-Lm = 4;
-Lc = 30; %300ms
+Lm = 1;
+Lc = 20; %300ms
 D = 3; %30ms
 F = 2;
 Iteration = 3;
-Epsilon = 1e-4;
+Epsilon = 1e-10;
 
 ch1File = './data/derev/metroplex_ch1.wav';
 ch2File = './data/derev/metroplex_ch2.wav';
@@ -66,41 +66,71 @@ spec = vertcat(ch1Spec, ch2Spec, ch3Spec, ch4Spec);
 Lc = ceil(F*frameSize*Lc/numBands);
 D = ceil(F*frameSize*D/numBands);
 
+Mu = zeros(Lm*Lc,1,size(ch1Spec,1));
+Fi = zeros(Lm*Lc,Lm*Lc,size(ch1Spec,1));
+K = zeros(Lm*Lc,1,size(ch1Spec,1));
+alpha = 0.99;
+
 X = zeros(Lm*Lc, 1);
 dCh1 = zeros(size(ch1Spec));
 
-for ii=1:size(ch1Spec, 1)
-    for iter = 1:Iteration
-        R = zeros(Lm*Lc);
-        P = zeros(Lm*Lc, 1);
-        for jj = (Lc+D):size(ch1Spec, 2)
-            if iter == 1
-                var_ch1 = max(ch1Spec(ii, jj).*conj(ch1Spec(ii, jj)), Epsilon);
-                var_ch2 = max(ch2Spec(ii, jj).*conj(ch2Spec(ii, jj)), Epsilon);
-                var_ch3 = max(ch3Spec(ii, jj).*conj(ch3Spec(ii, jj)), Epsilon);
-                var_ch4 = max(ch4Spec(ii, jj).*conj(ch4Spec(ii, jj)), Epsilon);
-                var(jj)=max([var_ch1 var_ch2 var_ch3 var_ch4]);
+
+% Streaming/Adaptive Process
+
+for bands_num = 1:size(ch1Spec, 1)
+    Fi(:,:,bands_num)=eye(Lm*Lc);
+end;
+
+for ii = Lc+D+1:size(ch1Spec, 2)
+    for bands_num = 1:size(ch1Spec, 1)
+        for ch_num = 0:Lm-1
+            for kk = 1:Lc
+                X(ch_num*Lc + kk)=spec(ch_num*size(ch1Spec,1)+bands_num,ii-D-kk);
             end;
-            for ll = 0:Lm-1
-                for kk = 1:Lc
-                    X(ll*Lc + kk)=spec(ll*size(ch1Spec,1)+ii,jj-D-kk+1);
-                end;
-            end;
-            R = R + ((X*X')./var(jj));
-            P = P + (X*conj(ch1Spec(ii,jj))./var(jj));
         end;
-        C=pinv(R)*P;
-        for jj = (Lc+D):size(ch1Spec, 2)
-            for ll = 0:Lm-1
-                for kk = 1:Lc
-                    X(ll*Lc + kk)=spec(ll*size(ch1Spec,1)+ii,jj-D-kk+1);
-                end;
-            end;
-            dCh1(ii, jj)=ch1Spec(ii, jj) - C'*X;
-            var(jj) = max(dCh1(ii, jj).*conj(dCh1(ii, jj)), Epsilon);
-        end;
+        dCh1(bands_num, ii)=ch1Spec(bands_num,ii)-Mu(:,:,bands_num)'*X;
+        var_ch1 = max(dCh1(bands_num,ii).*conj(dCh1(bands_num,ii)), 0);
+        
+        K(:,:,bands_num)=Fi(:,:,bands_num)*X/(alpha*var_ch1+X'*Fi(:,:,bands_num)*X);
+        Mu(:,:,bands_num)=Mu(:,:,bands_num)+K(:,:,bands_num)*conj(dCh1(bands_num,ii));
+        Fi(:,:,bands_num)=(Fi(:,:,bands_num)-K(:,:,bands_num)*X'*Fi(:,:,bands_num))/alpha;
     end;
 end;
+
+% Batch Process
+%
+% for ii=1:size(ch1Spec, 1)
+%     for iter = 1:Iteration
+%         R = zeros(Lm*Lc);
+%         P = zeros(Lm*Lc, 1);
+%         for jj = (Lc+D):size(ch1Spec, 2)
+%             if iter == 1
+%                 var_ch1 = max(ch1Spec(ii, jj).*conj(ch1Spec(ii, jj)), Epsilon);
+%                 var_ch2 = max(ch2Spec(ii, jj).*conj(ch2Spec(ii, jj)), Epsilon);
+%                 var_ch3 = max(ch3Spec(ii, jj).*conj(ch3Spec(ii, jj)), Epsilon);
+%                 var_ch4 = max(ch4Spec(ii, jj).*conj(ch4Spec(ii, jj)), Epsilon);
+%                 var(jj)=max([var_ch1 var_ch2 var_ch3 var_ch4]);
+%             end;
+%             for ll = 0:Lm-1
+%                 for kk = 1:Lc
+%                     X(ll*Lc + kk)=spec(ll*size(ch1Spec,1)+ii,jj-D-kk+1);
+%                 end;
+%             end;
+%             R = R + ((X*X')./var(jj));
+%             P = P + (X*conj(ch1Spec(ii,jj))./var(jj));
+%         end;
+%         C=pinv(R)*P;
+%         for jj = (Lc+D):size(ch1Spec, 2)
+%             for ll = 0:Lm-1
+%                 for kk = 1:Lc
+%                     X(ll*Lc + kk)=spec(ll*size(ch1Spec,1)+ii,jj-D-kk+1);
+%                 end;
+%             end;
+%             dCh1(ii, jj)=ch1Spec(ii, jj) - C'*X;
+%             var(jj) = max(dCh1(ii, jj).*conj(dCh1(ii, jj)), Epsilon);
+%         end;
+%     end;
+% end;
 
 ch1Out = InvDftFB(dCh1, frameSize, numBands, h, length(h)/numBands);
 audiowrite(out1File, ch1Out, fs);
